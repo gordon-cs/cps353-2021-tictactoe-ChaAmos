65bb1909b49b237377c04b509c0c315a77d674cc
amos-cha <amos.cha98@gmail.com>
2021-09-10 09:44:24 -0400

    update
    updated:
    JS:
    pseudo win condition
    CSS:
    changed circle icon
    color modification

--- script.js
+++ script.js
@@ -8,14 +8,70 @@ const gridSquares = document.querySelectorAll('.grid-square');
 let gameLive = true;
 let xNext = true;
 
+let win = null;
+
 
 
 //functions
 
 /*
- * Check win/tie condition
+ * Check win conditions & which marker is next
  */
-const condition = () => {
+const gameStatus = () => {
+  //column check
+  for (let col = 0; col < 3; col++) {
+    if (gridSquares[col].classList[2] && 
+      gridSquares[col].classList[2] === gridSquares[col+3].classList[2] &&
+      gridSquares[col+6].classList[2] === gridSquares[col+3].classList[2]) {
+        console.log(gridSquares[col].classList[2],'won');
+        gridSquares[col].classList.add('win');
+        gridSquares[col+3].classList.add('win');
+        gridSquares[col+6].classList.add('win');
+
+        gameLive = false;
+        win = gridSquares[col].classList[2];
+      };                            
+  };
+
+  //row check
+  for (let row = 0; row < 7; row+=3) {
+    if (gridSquares[row].classList[2] &&
+      gridSquares[row].classList[2] === gridSquares[row+1].classList[2] &&
+      gridSquares[row+1].classList[2] === gridSquares[row+2].classList[2]) {
+        console.log(gridSquares[row].classList[2],'won');
+        gridSquares[row].classList.add('win');
+        gridSquares[row+1].classList.add('win');
+        gridSquares[row+2].classList.add('win');
+
+        gameLive = false;
+        win = gridSquares[row].classList[2];
+      };
+  };
+
+  //diagonal check
+  if (gridSquares[0].classList[2] && 
+    gridSquares[0].classList[2] === gridSquares[4].classList[2] && 
+    gridSquares[4].classList[2] === gridSquares[8].classList[2]) {
+      console.log(gridSquares[0].classList[2],'won');
+      gridSquares[0].classList.add('win');
+      gridSquares[4].classList.add('win');
+      gridSquares[8].classList.add('win');
+
+      gameLive = false;
+      win = gridSquares[0].classList[2];
+    };
+
+  if (gridSquares[2].classList[2] && 
+    gridSquares[2].classList[2] === gridSquares[4].classList[2] && 
+    gridSquares[4].classList[2] === gridSquares[6].classList[2]) {
+      console.log(gridSquares[2].classList[2],'won');
+      gridSquares[2].classList.add('win');
+      gridSquares[4].classList.add('win');
+      gridSquares[6].classList.add('win');
+
+      gameLive = false;
+      win = gridSquares[2].classList[2];
+    };
 
 }
 
@@ -24,6 +80,7 @@ const condition = () => {
  */
 const rsGame = (e) => {
   console.log(e);
+
 };
 
 /*
@@ -32,7 +89,7 @@ const rsGame = (e) => {
 const markSquare = (e) => {
   cl = e.target.classList;
   sqNum = cl[1];
-  console.log("SQ#",sqNum);
+
   //if square is occupied
   if(cl[2]){
     console.log("taken",cl[2]);
@@ -41,11 +98,11 @@ const markSquare = (e) => {
 
   //marks the correct marker
   if(xNext){
-    console.log(e.target);
     cl.add('x');
   } else {
     cl.add('o');
   }
+  gameStatus();
   //swaps back
   xNext = !xNext;
 };
@@ -59,21 +116,6 @@ restart.addEventListener('click',rsGame);
 for(const sq of gridSquares) {
   sq.addEventListener('click',markSquare)
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*                                                                                                                                      
